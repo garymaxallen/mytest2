ffc3dad6ce39c5413c308bf5cea97c71e1de07b8
pcl <pcl@pcls-iMac-Pro.local>
2022-09-26 22:32:34 +0800

    xxx

--- mytest2/ViewController.swift
+++ mytest2/ViewController.swift
@@ -8,16 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let scrollView = UIScrollView()
+    let contentView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupScrollView()
+        setupViews()
+    }
+
+    func myfunc1() {
         NSLog("com.mycom.mytest2.log: %@", "fdasfasfasfasfasdffasa")
         view.backgroundColor = UIColor.systemGreen
         let width = UIScreen.main.bounds.size.width
         let height = UIScreen.main.bounds.size.height
-        let button1 = UIButton(frame: CGRect(x: width/3, y: height/2, width: 100, height: 30))
+        let button1 = UIButton(frame: CGRect(x: width / 3, y: height / 2, width: 100, height: 30))
         button1.backgroundColor = UIColor.systemBlue
-        view.addSubview(button1)
+
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.addSubview(button1)
+
+        view.addSubview(scrollView)
         NSLog("com.mycom.mytest2.log: %@", button1.backgroundColor ?? "<nil>")
+    }
+
+    func setupScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+
+        // scrollView.contentInsetAdjustmentBehavior = .never
+        // scrollView.contentInset = UIEdgeInsets.zero
+        // scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+        // scrollView.contentOffset = CGPoint(x: 0.0, y: 0.0)
+    }
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    func setupViews() {
+        contentView.addSubview(titleLabel)
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3 / 4).isActive = true
+        contentView.addSubview(subtitleLabel)
+        subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
+        subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3 / 4).isActive = true
+        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
 }
