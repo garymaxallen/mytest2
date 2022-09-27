8e36261e626f5dba8a5941102fe4e623f8b4d89e
pcl <pcl@pcls-iMac-Pro.local>
2022-09-27 12:10:37 +0800

    xxx

--- mytest2/SceneDelegate.swift
+++ mytest2/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let winScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: winScene)
         window?.rootViewController = ViewController()
+        // window?.rootViewController = UINavigationController(rootViewController: ViewController())
         window?.backgroundColor = UIColor.systemBackground
         window?.makeKeyAndVisible()
     }
--- mytest2/ViewController.swift
+++ mytest2/ViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let scrollView = UIScrollView()
-    let contentView = UIView()
+    // let scrollView = UIScrollView()
+    // let contentView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setupScrollView()
-        setupViews()
+
+        // setScrollView1()
+        // listFont()
+        setTextView()
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
     func myfunc1() {
@@ -33,54 +39,83 @@ class ViewController: UIViewController {
         NSLog("com.mycom.mytest2.log: %@", button1.backgroundColor ?? "<nil>")
     }
 
-    func setupScrollView() {
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.translatesAutoresizingMaskIntoConstraints = false
+    func listFont() {
+        for family in UIFont.familyNames {
+            for font in UIFont.fontNames(forFamilyName: family) {
+                NSLog("com.mycom.mytest2.log: family: %@ font: %@", family, font)
+            }
+        }
+    }
+
+    func setScrollView2() {}
+
+    func setTextView() {
+        // let textView = UITextView(frame: CGRect(x: 20.0, y: 90.0, width: 250.0, height: 100.0))
+        let textView = UITextView()
+        view.addSubview(textView)
+
+        textView.backgroundColor = UIColor.systemBackground
+        textView.contentInsetAdjustmentBehavior = .automatic
+        textView.center = view.center
+        textView.textAlignment = NSTextAlignment.justified
+        textView.textColor = UIColor.black
+        textView.font = UIFont(name: "Consolas", size: 24)
+
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+
+    func setScrollView1() {
+        let scrollView = UIScrollView()
+        let contentView = UIView()
+
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            label.numberOfLines = 0
+            label.sizeToFit()
+            label.textColor = UIColor.black
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+
+        let subtitleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?  +++++++++++++++++++ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?  +++++++++++++++++     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            label.numberOfLines = 0
+            label.sizeToFit()
+            label.textColor = UIColor.black
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
 
-        // scrollView.contentInsetAdjustmentBehavior = .never
-        // scrollView.contentInset = UIEdgeInsets.zero
-        // scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
-        // scrollView.contentOffset = CGPoint(x: 0.0, y: 0.0)
-    }
-
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        label.numberOfLines = 0
-        label.sizeToFit()
-        label.textColor = UIColor.black
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    let subtitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-        label.numberOfLines = 0
-        label.sizeToFit()
-        label.textColor = UIColor.black
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    func setupViews() {
         contentView.addSubview(titleLabel)
         titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3 / 4).isActive = true
+
         contentView.addSubview(subtitleLabel)
         subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
+        // subtitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
         subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3 / 4).isActive = true
         subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
